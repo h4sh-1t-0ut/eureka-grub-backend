cc2d1ebf1a7bfcad0bb7ba72f92c092304cee78e
immikeoa <immikeoa@gmail.com>
2023-07-20 14:25:07 -0400

    adds CRUD functionality and test for the recipe backend.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ApplicationRecord
     belongs_to :user
-    validates :name, :instructions, :cook_time, :ingredients, :course, :servings, :allergies, :tutorial_link, :recipe_image, presence: true
+    validates :name, :instructions, :cook_time, :ingredients, :course, :servings, :allergies, :tutorial_link, :recipe_image,:user_id, presence: true
 end
